9c54e77a1bea439babd1f8656b4e8793852344de
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2023-12-24 16:35:25 +0300

    UPDATE: добавить событие result

--- homeworks/4-1-calc-events/index.js
+++ homeworks/4-1-calc-events/index.js
@@ -1,59 +1,50 @@
-const EventEmitter = require("events")
-const isNumber = require("./isNumber.js")
-const addition = require("./addition.js")
-const subtraction = require("./subtraction.js")
-const multiply = require("./multiply.js")
-const division = require("./division.js")
+const EventEmitter = require("events");
+const isNumber = require("./isNumber.js");
+const addition = require("./addition.js");
+const subtraction = require("./subtraction.js");
+const multiply = require("./multiply.js");
+const division = require("./division.js");
 
 function main() {
-    const event = new EventEmitter()
+    const event = new EventEmitter();
 
-    let firstNumber = process.argv[2]
-    let secondNumber = process.argv[3]
-    const command = process.argv[4]
+    let firstNumber = process.argv[2];
+    let secondNumber = process.argv[3];
+    const command = process.argv[4];
 
     if (!isNumber(firstNumber, 1) || !isNumber(secondNumber, 2)) {
-        return false
+        return false;
     }
 
-    firstNumber = Number(firstNumber)
-    secondNumber = Number(secondNumber)
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
 
-    event.on('add', () => {
-        console.log(`Сумма равна: ${addition(firstNumber, secondNumber)}`)
-    })
-    event.on('subtraction', () => {
-        console.log(`Разность равна: ${subtraction(firstNumber, secondNumber)}`)
-    })
-    event.on('multiply', () => {
-        console.log(`Умножение равно: ${multiply(firstNumber, secondNumber)}`)
-    })
-    event.on('division', () => {
-        console.log(`Деление равно: ${division(firstNumber, secondNumber)}`)
-    })
+    event.on("addition", (f, s) => {
+        event.emit("result", addition(f, s));
+    });
+    event.on("subtraction", (f, s) => {
+        event.emit("result", subtraction(f, s));
+    });
+    event.on("multiply", (f, s) => {
+        event.emit("result", multiply(f, s));
+    });
+    event.on("division", (f, s) => {
+        event.emit("result", division(f, s));
+    });
+    event.on("result", (result) => {
+        console.log(`Результат: ${result}`);
+    });
 
-    switch (command) {
-        case 'add':
-            event.emit('add')
-            break;
-        case 'subtraction':
-            event.emit('subtraction')
-            break;
-        case 'multiply':
-            event.emit('multiply')
-            break;
-        case 'division':
-            event.emit('division')
-            break;
-        default:
-            console.log("Неизвестная команда")
-            break;
+    if (
+        command === "addition" ||
+        command === "subtraction" ||
+        command === "multiply" ||
+        command === "division"
+    ) {
+        event.emit(command, firstNumber, secondNumber);
+    } else {
+        console.log("Неизвестная команда");
     }
-
-    event.removeAllListeners("add")
-    event.removeAllListeners("subtraction")
-    event.removeAllListeners("multiply")
-    event.removeAllListeners("division")
 }
 
-main()
+main();
