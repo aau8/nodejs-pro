b653cdc113cb259165e958424822d8a83903df3b
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-01-12 15:11:42 +0300

    ADD: module os

---
+++ modules/os/app.js
@@ -0,0 +1,12 @@
+/**
+ * # Module OS
+ *
+ * - [ ] os.EOL
+ * - [ ] os.arch()
+ * - [ ] os.homedir()
+ */
+
+const os = require("os")
+const path = require("path")
+
+console.log(os.homedir(), path.sep)
