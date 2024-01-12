26caac27f3b0e8f1d67ebb6d670e7f3861116ac2
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-01-12 15:13:46 +0300

    UPDATE: weather-cli

--- demos/7-npm/weather-cli/index.js
+++ demos/7-npm/weather-cli/index.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js"
 import logService from "./services/log.service.js"
+import { saveKeyValue } from "./services/storage.service.js"
+
 
 const main = () => {
     const args = getArgs(process.argv)
     // console.log(args)
-    logService.success("is done")
+    // logService.success(process.arch)
+
     if (args.h) {
         logService.help()
     } else if (args.c) {
         // сохранить город
     } else if (args.t) {
         // сохранить токен
+        saveKeyValue("TOKEN", args.t)
     }
 }
 
