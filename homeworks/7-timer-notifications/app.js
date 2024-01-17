e36a32f062819286153e3d7c7a2f2f3096c75caf
Artyom Ugryumov <artiom.ygryumov@gmail.com>
2024-01-17 10:39:47 +0300

    UPDATE: 7-timer-notifications сделать правки ментора

--- homeworks/7-timer-notifications/app.js
+++ homeworks/7-timer-notifications/app.js
@@ -15,13 +15,6 @@ const secTotal = (hours * 60 * 60 + minutes * 60 + seconds)
 let secTime = 0
 
 function calcTime() {
-    const remainSecTime = secTotal - secTime
-    const remainHours = Math.trunc(remainSecTime / 60 / 60)
-    const remainMinutes = Math.trunc((remainSecTime - (remainHours * 60 * 60)) / 60)
-    const remainSeconds = Math.trunc(remainSecTime - ((remainHours * 60 + remainMinutes) * 60))
-
-    console.log(`${remainHours}h ${remainMinutes}m ${remainSeconds}s`)
-
     if (secTime >= secTotal) {
         clearInterval(intervalId)
         console.log(message)
